6d6eb03c6048335e6ddcb7d4c0aa9aa7cbdd79e5
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-10-30 23:28:49 +0700

    fix title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tech Station - Good things in heer.' });
+  res.render('index', { title: 'Tech Station - Good things in here.' });
 });
 
 module.exports = router;
